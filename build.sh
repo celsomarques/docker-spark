f7ff73fb9937db17e6d71095543a896b92b68b1a
Celso Marques <celso.marques@gsw.com.br>
2021-01-07 11:26:26 -0300

    Update version

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TAG=2.4.5-hadoop2.7
+TAG=2.4.7-hadoop2.7
 
 build() {
     NAME=$1
-    IMAGE=bde2020/spark-$NAME:$TAG
+    IMAGE=celsomarques/spark-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
